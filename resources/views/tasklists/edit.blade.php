d70415852ab095b32dd7166933a1317ffaffc3ea
EC2 Default User <ec2-user@ip-172-31-14-18.us-east-2.compute.internal>
2018-05-24 06:51:32 +0000

    fix Bootstrap form

--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -4,21 +4,26 @@
 
     <h1>id: {{ $tasklist->id }} タスク編集ページ</h1>
 
-    {!! Form::model($tasklist, ['route' => ['tasks.update', $tasklist->id], 'method' => 'put']) !!}
+        <div class="row">
+            <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 ">
+        
+                {!! Form::model($tasklist, ['route' => ['tasks.update', $tasklist->id], 'method' => 'put']) !!}
     
-        <div class="form-group">
-            {!! Form::label('stasus', 'ステータス:') !!}
-            {!! Form::text('status') !!}
-        </div>
+                    <div class="form-group">
+                        {!! Form::label('stasus', 'ステータス:') !!}
+                        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                    </div>
     
-        <div class="form-group">
-            {!! Form::label('content', 'タスク:') !!}
-            {!! Form::text('content') !!}
+                    <div class="form-group">
+                        {!! Form::label('content', 'タスク:') !!}
+                        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    </div>
+
+                    {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+
+                {!! Form::close() !!}
+            
+            </div>
         </div>
-
-        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
-
-    {!! Form::close() !!}
-
-
+        
 @endsection
